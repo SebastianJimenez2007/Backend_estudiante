e4a6cce13a25fdde855e08b4feed3164fc5a5656
Sebastian Jimenez <sebastianjimenezjjb@gmail.com>
2025-09-28 22:47:53 -0500

    first commit

---
+++ estudiante/estudiante/src/test/java/com/backend/estudiante/EstudianteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.estudiante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudianteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
